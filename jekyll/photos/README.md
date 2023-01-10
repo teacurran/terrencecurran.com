51591bddf21234025022cca0ce26e6bc63908c04
Terrence Curran <tcurran@approachingpi.com>
2023-01-10 15:09:33 -0500

    webpack

---
+++ jekyll/photos/README.md
@@ -0,0 +1,1 @@
+photo galleries go here, sorted by directory.
---
+++ jekyll/webpack.config.js
@@ -0,0 +1,43 @@
+const path = require('path');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
+
+// https://webpack.js.org/configuration/
+module.exports = {
+    plugins: [new MiniCssExtractPlugin({
+            filename: '../css/[name]-[contenthash].css'
+        }
+    )],
+    externals: {
+        jquery: 'jQuery',
+    },
+    entry: {
+        main: path.join(__dirname, 'js', 'main'),
+    },
+    output: {
+        path: path.resolve(__dirname, 'js'),
+        filename: '[name]-[contenthash].js',
+    },
+    resolve: {
+        extensions: ['.json', '.js', '.jsx'],
+        modules: ['node_modules'],
+    },
+    optimization: {
+      minimize: true,
+      minimizer: [
+          new TerserPlugin(),
+          new CssMinimizerPlugin(),
+      ],
+    },
+    module: {
+        rules: [
+            { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader' },
+            {
+              test: /.s?css$/,
+              use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+            }
+        ],
+    },
+};
