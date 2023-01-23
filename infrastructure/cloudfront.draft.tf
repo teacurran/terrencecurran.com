78bf4c9919129b3fab08a0bf95828a2f42eefcb0
Tea Curran <tea@approachingpi.com>
2023-01-23 12:27:16 -0500

    cloudfront + build script for draft

---
+++ infrastructure/cloudfront.draft.tf
@@ -0,0 +1,62 @@
+resource "aws_cloudfront_distribution" "draft" {
+  origin {
+    domain_name = aws_s3_bucket_website_configuration.draft_bucket.website_endpoint
+    origin_id   = "s3-${local.draft_domain}"
+    custom_origin_config {
+      http_port              = 80
+      https_port             = 443
+      origin_protocol_policy = "http-only"
+      origin_ssl_protocols   = ["TLSv1", "TLSv1.1", "TLSv1.2"]
+    }
+  }
+
+  enabled         = true
+  is_ipv6_enabled = true
+
+  # This alias only covers the root domain, others will forward
+  aliases = [local.draft_domain]
+
+  custom_error_response {
+    error_caching_min_ttl = 0
+    error_code            = 404
+    response_code         = 200
+    response_page_path    = "/404.html"
+  }
+
+  default_cache_behavior {
+    allowed_methods  = ["GET", "HEAD"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = "s3-${local.draft_domain}"
+
+    forwarded_values {
+      query_string = true
+
+      cookies {
+        forward = "none"
+      }
+
+      headers = ["Origin"]
+    }
+
+    viewer_protocol_policy = "redirect-to-https"
+    min_ttl                = 0
+    default_ttl            = 86400
+    max_ttl                = 31536000
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  viewer_certificate {
+    acm_certificate_arn      = aws_acm_certificate_validation.cert_validation.certificate_arn
+    ssl_support_method       = "sni-only"
+    minimum_protocol_version = "TLSv1.2_2018"
+  }
+
+  tags = local.tags
+}
+
+
